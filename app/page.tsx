43a0606a706b06ac8c13de3b1456fac13973c1c6
Doni6519 <meshackdaniel225@gmail.com>
2025-05-18 23:31:56 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,117 @@
+"use client";
+import { ChevronLeft, ChevronRight, X } from "lucide-react";
 import Image from "next/image";
+import React, { useState } from "react";
 
-export default function Home() {
+// Save all images in an array
+const images = [
+  {
+    src: "/images/image-1.png",
+    alt: "1st Image",
+  },
+  {
+    src: "/images/image-2.png",
+    alt: "2nd Image",
+  },
+  {
+    src: "/images/image-3.png",
+    alt: "3rd Image",
+  },
+  {
+    src: "/images/image-4.png",
+    alt: "4th Image",
+  },
+  {
+    src: "/images/image-5.png",
+    alt: "5th Image",
+  },
+  {
+    src: "/images/image-6.png",
+    alt: "6th Image",
+  },
+];
+
+const home = () => {
+  const [galleryImage, setGalleryImage] = useState("/images/image-1.png");
+  const [showImage, setShowImage] = useState("hidden");
+
+  // Create a function to display next image
+  const [index, setIndex] = useState(0);
+  const nextImage = async () => {
+    if (index + 1 == images.length) {
+      await setIndex(0);
+      setGalleryImage(images[0].src);
+    } else {
+      await setIndex(index + 1);
+      setGalleryImage(images[index + 1].src);
+    }
+  };
+
+  // Create a function to display previous image
+  const prevImage = async () => {
+    if (index == 0) {
+      await setIndex(images.length -1);
+      setGalleryImage(images[images.length - 1].src);
+    } else {
+      await setIndex(index - 1);
+      setGalleryImage(images[index - 1].src);
+    }
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="w-5/6 mx-auto py-10">
+      <h1 className="font-bold text-4xl text-center">Image Gallery</h1>
+      {/* Map all images in a grid display */}
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 mt-20">
+        {images.map((image, i) => (
+          <div
+            key={i}
+            className="overflow-hidden rounded-xl cursor-pointer"
+            onClick={() => {
+              setShowImage("");
+              setGalleryImage(image.src);
+              setIndex(i);
+            }}
           >
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src={image.src}
+              width={1000}
+              height={800}
+              alt={image.alt}
+              className="hover:scale-105"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
+        ))}
+      </div>
+      {/* Create container for gallery */}
+      <div
+        className={`fixed h-screen w-full left-0 top-0 py-40 md:py-28 lg:py-10 px-5 lg:px-40 bg-white flex items-center ${showImage}`}
+      >
+        <div onClick={prevImage} className="hover:scale-105 rounded-full cursor-pointer p-2 absolute left-0.5 lg:relative bg-blue-900 md:me-10">
+          <ChevronLeft className="w-10 h-10" color="white" />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Image
+          src={galleryImage}
+          width={1000}
+          height={800}
+          alt="1st Image"
+          className="block w-full h-full rounded-4xl object-cover"
+        />
+        <div
+          onClick={nextImage}
+          className="hover:scale-105 rounded-full cursor-pointer p-2 absolute left-0.5 lg:relative bg-blue-900 md:ms-10"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <ChevronRight className="w-10 h-10" color="white" />
+        </div>
+      </div>
+      <div
+        onClick={() => setShowImage("hidden")}
+        className={`hover:scale-105 fixed top-10 right-5 lg:right-10 rounded-full cursor-pointer p-2 bg-red-900 ${showImage}`}
+      >
+        <X className="w-10 h-10" color="white" />
+      </div>
     </div>
   );
-}
+};
+
+export default home;
